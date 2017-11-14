210673aaba55451e93199c4f7868102a31742434
haohong725 <haohong725@qq.com>
2017-11-14 18:30:16 +0800

    mybatis demo

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,10 +2,23 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    private final CityMapper cityMapper;
+
+    public Application(CityMapper cityMapper) {
+        this.cityMapper = cityMapper;
+    }
+
+    @Bean
+    CommandLineRunner sampleCommandLineRunner() {
+        return (args) -> System.out.println(this.cityMapper.findByState("CA"));
     }
 }
