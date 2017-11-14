15978a5e18d5e630d6e5de4f5b24103b82c40892
haohong725 <haohong725@qq.com>
2017-11-14 18:45:41 +0800

    mybatis demo beta

--- src/main/java/com/controllers/GreetingController.java
+++ src/main/java/com/controllers/GreetingController.java
@@ -1,16 +1,32 @@
 package com.controllers;
 
+import com.City;
+import com.CityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@Controller("/greeting")
+@Controller("GreetingController")
 public class GreetingController {
+
+    @Autowired
+    private CityMapper cityMapper;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+
+    @RequestMapping("/city")
+    public String city(@RequestParam(value="city", defaultValue="CA") String city, Model model) {
+        City cityEntity = cityMapper.findByState(city);
+        model.addAttribute("city", cityEntity);
+
+        return "city";
+    }
+
 }
