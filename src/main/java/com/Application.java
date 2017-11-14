ec956a5325a74317f31de1b963ecf49cde3140eb
haohong725 <haohong725@qq.com>
2017-11-14 18:47:12 +0800

    移调多余代码

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -11,14 +11,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    private final CityMapper cityMapper;
-
-    public Application(CityMapper cityMapper) {
-        this.cityMapper = cityMapper;
-    }
-
-    @Bean
-    CommandLineRunner sampleCommandLineRunner() {
-        return (args) -> System.out.println(this.cityMapper.findByState("CA"));
-    }
+//    private final CityMapper cityMapper;
+//
+//    public Application(CityMapper cityMapper) {
+//        this.cityMapper = cityMapper;
+//    }
+//
+//    @Bean
+//    CommandLineRunner sampleCommandLineRunner() {
+//        return (args) -> System.out.println(this.cityMapper.findByState("CA"));
+//    }
 }
