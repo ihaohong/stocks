30e4a3d8ffb1e2bb8d8a35c072bbde94c190a9be
郝鸿 <hom.hao@tophant.com>
2017-11-15 18:33:09 +0800

    spring security 与 数据库整合

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -1,7 +1,9 @@
 package com.services;
 
+import com.mappers.UserMapper;
+import com.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,18 +12,21 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import java.util.Collections;
 
 public class UserService implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String password = "huaerjie001";
+        User loginUser = userMapper.findByUsername(username);
 
-        password = new StandardPasswordEncoder().encode(password);
+        if (loginUser == null) {
+            throw new UsernameNotFoundException("user not found");
+        }
 
-        String role = "ROLE_USER";
-
-        return new User(
-                username,
-                password,
-                Collections.singleton(new SimpleGrantedAuthority(role))
+        return new org.springframework.security.core.userdetails.User(
+                loginUser.getUsername(),
+                loginUser.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority(loginUser.getRole()))
         );
     }
 }
