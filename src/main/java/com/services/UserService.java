48034dff145b392428de539fce809fb25d2cd9fb
郝鸿 <hom.hao@tophant.com>
2017-11-15 18:35:06 +0800

    x

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import java.util.Collections;
 
