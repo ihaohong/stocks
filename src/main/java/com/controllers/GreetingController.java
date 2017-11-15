de37c00ab7be8dfbb113b9adfb03225a1ed10eab
郝鸿 <hom.hao@tophant.com>
2017-11-15 17:37:14 +0800

    add mappers, models package

--- src/main/java/com/controllers/GreetingController.java
+++ src/main/java/com/controllers/GreetingController.java
@@ -1,7 +1,7 @@
 package com.controllers;
 
-import com.City;
-import com.CityMapper;
+import com.models.City;
+import com.mappers.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/mappers/CityMapper.java
+++ src/main/java/com/mappers/CityMapper.java
@@ -1,5 +1,6 @@
-package com;
+package com.mappers;
 
+import com.models.City;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
