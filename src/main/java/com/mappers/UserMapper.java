26957396ac19565ded9eaa3ad9851e024a5c3d19
郝鸿 <hom.hao@tophant.com>
2017-11-17 10:24:41 +0800

    User Mapper

--- src/main/java/com/mappers/UserMapper.java
+++ src/main/java/com/mappers/UserMapper.java
@@ -1,16 +1,16 @@
 package com.mappers;
 
 import com.models.User;
-import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+//import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT id, username, password, role FROM users WHERE username = #{username} LIMIT 1")
+//    @Select("SELECT id, username, password, role FROM users WHERE username = #{username} LIMIT 1")
     User findByUsername(String username);
 
-    @Insert("INSERT INTO users(username, password, role) VALUES(#{username}, #{password}, #{role} ) ")
+//    @Insert("INSERT INTO users(username, password, role) VALUES(#{username}, #{password}, #{role} ) ")
     void insertUser(User user);
 }
