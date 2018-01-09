e2da6d1b0773bab19933583ac7c0ac4ee2acc230
haohong725 <haohong725@qq.com>
2018-01-09 23:15:37 +0800

    移除Spring Security

--- src/main/java/com/controllers/IndexController.java
+++ src/main/java/com/controllers/IndexController.java
@@ -1,13 +1,20 @@
 package com.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("IndexController")
 public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     public String index(Model model) {
+        logger.info("index accessed");
+
         model.addAttribute("foo", "bar");
 
         return "index/index";
--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -3,43 +3,11 @@ package com.services;
 import com.mappers.UserMapper;
 import com.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import java.util.Collections;
 
-public class UserService implements UserDetailsService {
+public class UserService {
     @Autowired
     private UserMapper userMapper;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User loginUser = userMapper.findByUsername(username);
-
-        if (loginUser == null) {
-            throw new UsernameNotFoundException("user not found");
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-                loginUser.getUsername(),
-                loginUser.getPassword(),
-                Collections.singleton(new SimpleGrantedAuthority(loginUser.getRole()))
-        );
-    }
-
-    public User registerUser(String username, String password) {
-        String encodedPassword = new StandardPasswordEncoder().encode(password);
-
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(encodedPassword);
-        user.setRole("ROLE_USER");
-
-        userMapper.insertUser(user);
-
-        return user;
-    }
 }
